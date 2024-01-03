987c99ba088d678fb691b7c26c383bdc44870702
MatDat <math03t7@stud.kea.dk>
2024-01-03 06:10:30 +0100

    Initial commit

---
+++ src/main/java/com/example/testtileksamen/TestTilEksamenApplication.java
@@ -0,0 +1,13 @@
+package com.example.testtileksamen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTilEksamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTilEksamenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testtileksamen/TestTilEksamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testtileksamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTilEksamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
